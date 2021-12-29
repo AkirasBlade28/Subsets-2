bcc6cb5592232d9a2e87159b7faf9b6f281acf82
Miloš Janković <95304147+AkirasBlade28@users.noreply.github.com>
2021-12-29 19:18:32 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,13 @@ Input: nums = [0]
 Output: [[],[0]]
  */
 public class Main {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	int [] n = {1,2,3,4,5};
-      System.out.println(subsetsWithDup(n));
+		int [] n = {1,2,3,4,1};
+    	System.out.println(subsetsWithDup(n));
 	}
-	public static List<List<Integer>> subsetsWithDup(int[] nums) { // original int[] type
+	public static List<List<Integer>> subsetsWithDup(int[] nums) { 
 	
 		List <List<Integer>> subsList = new ArrayList <>();
 		
@@ -27,12 +28,12 @@ public class Main {
 			subsList.add(new ArrayList<>());
 			subsList.add(new ArrayList<>());
 			subsList.get(1).add(nums[0]);	
-			return subsList;	// 3 len. in. == multidim.arr. [[6]]
-		}
+			return subsList;	
+			}
 		else if (nums.length > 1) {
 			for(int i = 0; i<nums.length*2; ++i) {
 				subsList.add(new ArrayList<>());
-			}			// when [0] == 1 len. * 2 [[2len.]]-[[][0]]	
+			}							// when [0] == 1 len. * 2 [[2len.]]-[[][0]]	
 		int restriction = nums.length; // - Each set is a subset of itself or an empty subset
 		int r=1;											
 		for(int i=0; i<nums.length; ++i) {
@@ -45,8 +46,30 @@ public class Main {
 					}
 				++r;
 			   }
-			}	
+			}
+		check(nums, subsList);
 		}
+		
 		return subsList;
+	}
+
+	private static void check(int[] nums, List<List<Integer>> subsList) {
+		// trim + rest of numbers set (if not duplicate+check for duplicate)
+		List<List<Integer>> l = new ArrayList<>();
+		for(int i=0; i<nums.length;++i) {
+			l.add(new ArrayList<>());
+			l.get(i).add(nums[i]);
+		}
+		for(int i=0; i<subsList.size();++i) {
+			for(List e : l) {
+				if(!subsList.contains(e)) {
+					subsList.add(e);	
+				}
+			}
+			if(i>1) // check and remove duplicate single number in ar
+			if(subsList.get(1).equals(subsList.get(i))) {
+				subsList.remove(i);
+			}
 		}
 	}
+}
